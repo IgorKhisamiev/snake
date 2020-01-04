18160b83b22d77184c645bbfd375ced395d3e390
Igor Khisamiev <riks@2bim.ru>
2020-01-05 01:52:16 +0300

    Создание класса Point

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,21 +10,32 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            int x1 = 33;
-            int y1 = 3;
-            char sym1 = '*';
-            Draw(x1, y1, sym1);
-            Draw(5, 5, '&');
+            //int x1 = 33;
+            //int y1 = 3;
+            //char sym1 = '*';
+
+            Point p1= new Point();
+            Point p2 = new Point();
+            p1.sym = '*';
+            p1.x = 3;
+            p1.y = 5;
+            //p1.Draw();
+
+
+            p2.Draw(5, 5, '&');
+            
+            //Draw(x1, y1, sym1);
+            //Draw(5, 5, '&');
 
             Console.ReadKey();
         }
 
-        static void Draw(int x1, int y1, char sym1)
-        {
-            Console.BackgroundColor = ConsoleColor.White;
-            Console.ForegroundColor = ConsoleColor.DarkBlue;
-            Console.SetCursorPosition(x1, y1);
-            Console.Write(sym1);
-        }
+        //static void Draw(int x1, int y1, char sym1)
+        //{
+        //    Console.BackgroundColor = ConsoleColor.White;
+        //    Console.ForegroundColor = ConsoleColor.DarkBlue;
+        //    Console.SetCursorPosition(x1, y1);
+        //    Console.Write(sym1);
+        //}
     }
 }
