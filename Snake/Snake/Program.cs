6335bab40bf6946866fd74c62b2ff52b9084063d
Igor Khisamiev <riks@2bim.ru>
2020-01-05 01:34:58 +0300

    Процедурный подход. Функция Draw

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,8 +10,21 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi");
+            int x1 = 33;
+            int y1 = 3;
+            char sym1 = '*';
+            Draw(x1, y1, sym1);
+            Draw(5, 5, '&');
+
             Console.ReadKey();
+        }
+
+        static void Draw(int x1, int y1, char sym1)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.SetCursorPosition(x1, y1);
+            Console.Write(sym1);
         }
     }
 }
